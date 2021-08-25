b46e34d1835c32b8cd1b0e49388bbedc9646dd0d
AngelaTaylor87 <82851741+AngelaTaylor87@users.noreply.github.com>
2021-08-25 12:05:33 -0500

    added server

---
+++ server.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+// Initializes Sequelize with session store
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {
+    maxAge: 3600,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () =>
+    console.log(
+      `\nServer running on port ${PORT}. Visit http://localhost:${PORT} and create an account!`
+    )
+  );
+});
