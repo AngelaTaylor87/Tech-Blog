874cf7c6785e08e95fbd50f32070c7ff1d20982d
AngelaTaylor87 <82851741+AngelaTaylor87@users.noreply.github.com>
2021-10-20 15:06:36 -0500

    updated api

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,11 +1,22 @@
 const router = require('express').Router();
 const { Post, User, Comment } = require('../models');
+const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
-    
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['email'],
+        }
+      ]
+    });
+
+    const posts = postData.map((post) => post.get({ plain: true }));
+
     res.render('homepage', {
-     
+      posts,
       logged_in: req.session.logged_in
     });
   } catch (err) {
@@ -15,32 +26,43 @@ router.get('/', async (req, res) => {
 
 router.get('/post/:id', async (req, res) => {
   try {
-const postData = Post.findByPk(req.params.id, {
-  include: [
-    {
-      model: User,
-      attributes: ['name'],
-    },
-    {
-      model: Comment,
-      attributes: ['id', 'comment', 'post_id', 'user_id', 'created_at'],
-      include: {
-        model: User,
-        attributes: ['name']
-      }
-    },
-  ],
-});
+    const postData = Post.findOne({where: { id: req.params.id },
+      include: [
+        {
+          model: Comment
+        }]
+    })
 
     const post = postData.get({ plain: true });
-    res.render('single-post', {
-      post,
+
+    res.render('post', {
+      ...post,
       logged_in: req.session.logged_in
     });
+
   } catch (err) {
     res.status(500).json(err);
   }
 });
+
+router.get('dashboard', withAuth, async (req, res) => {
+  try {
+    const userData = await User.findOne
+    ({
+      where: { id: req.session.user_id, },
+      attributes: { exclude: ['password'] },
+      include: [{ model: Post }],
+    });
+    const user = userData.get({ plain: true });
+    
+    res.render('dashboard', { ...user, logged_in: true 
+    });
+  
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 
 router.get('/login', (req, res) => {
   if (req.session.logged_in) {
@@ -49,16 +71,6 @@ router.get('/login', (req, res) => {
   }
   res.render('login');
 });
-router.get('/signup', (req, res) => {
-  if (req.session.logged_in) {
-    res.redirect('/profile');
-    return;
-  }
-  res.render('signup');
-});
 
-router.get('/post', (req, res) => {
-  res.render('post')
-})
 
 module.exports = router;
