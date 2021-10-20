d897c43e47f476be405d065b64acc6e8e224f3ef
AngelaTaylor87 <82851741+AngelaTaylor87@users.noreply.github.com>
2021-10-20 13:21:12 -0500

    fixed bugs

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,18 +3,9 @@ const { Post, User, Comment } = require('../models');
 
 router.get('/', async (req, res) => {
   try {
-    const postData = await Post.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ['name']
-        },
-      ],
-    });
-
-    const posts = postData.map((post) => post.get({ plain: true }));
+    
     res.render('homepage', {
-      posts,
+     
       logged_in: req.session.logged_in
     });
   } catch (err) {
@@ -65,5 +56,9 @@ router.get('/signup', (req, res) => {
   }
   res.render('signup');
 });
+
+router.get('/post', (req, res) => {
+  res.render('post')
+})
 
 module.exports = router;
